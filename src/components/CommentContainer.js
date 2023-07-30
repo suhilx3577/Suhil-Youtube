068e0f0103bc373b8c04abf402d0c1b2dff43a10
suhilx3577 <suhilx3577@gmail.com>
2023-07-31 03:22:40 +0530

    Revert "Built Live Chat Feature with API Polling"
    This reverts commit 4b97265a52ca5ddf9dd3468c23ff0870959fb4bd.

--- src/components/CommentContainer.js
+++ src/components/CommentContainer.js
@@ -135,15 +135,14 @@ const CommentContainer = () => {
     const CommentList = ({comments}) => {
         return (
             comments.map((comment,i)=>(
-            <div key={i} >
-                 <Comment data={comment}/>
+            <div>
+                 <Comment key={i} data={comment}/>
                  <div className='pl-5 border border-l-black ml-5'>
                     <CommentList comments={comment.replies}/>
                  </div>
             </div>
-        )));
-
-    };
+        )))
+    }
   return (
     <div className='w-4/5'>
         <h1 className='text-3xl font-bold'>Comments</h1>
